74f24231b223410b4e1766c97501cf1200fd1602
Oleh Parfeniuk <oleh.parfeniuk@artpix3d.com>
2023-07-17 14:13:28 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PreviewModule from "artpix-3d-preview";
+
+function App() {
+  return (
+    <div className="App">
+      <PreviewModule
+          id="28537352-7eac-489b-aa53-161973321b55"
+          env="https://orderarchive.okd.artpix3d.com"
+          isMobile
+      />
+    </div>
+  );
+}
+
+export default App;
